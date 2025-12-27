b45b589d7d6cab6da26ac7462b5143b5aff8d976
mustafaev.ib <tusxdie@gmail.com>
2025-12-28 02:41:05 +0700

    Cleaning

--- .config/sketchybar/plugins/cpu.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-CPU_USAGE=$(ps -A -o %cpu | awk '{s+=$1} END {print s}')
-CPU_USAGE=$(printf "%.0f" $CPU_USAGE)
-
-sketchybar --set $NAME label="${CPU_USAGE}%"
--- .config/sketchybar/plugins/memory.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-MEMORY_USAGE=$(memory_pressure | grep "System-wide memory free percentage:" | awk '{print 100-$5}' | sed 's/%//')
-
-if [ -z "$MEMORY_USAGE" ]; then
-    MEMORY_USAGE=$(vm_stat | awk '/Pages active/ {active=$3} /Pages wired/ {wired=$4} /Pages occupied/ {occupied=$5} /Pages free/ {free=$3} END {printf "%.0f", (active+wired+occupied)/(active+wired+occupied+free)*100}')
-fi
-
-sketchybar --set $NAME label="${MEMORY_USAGE}%"
